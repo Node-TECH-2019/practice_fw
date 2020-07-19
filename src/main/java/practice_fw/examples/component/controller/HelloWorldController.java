37e928c690bb0f5fced09c8b0a2db0911837bf08
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2020-07-20 01:32:32 +0900

    spring-context.xmlを手直し

--- src/main/java/practice_fw/examples/component/controller/HelloWorldController.java
+++ src/main/java/practice_fw/examples/component/controller/HelloWorldController.java
@@ -1,8 +1,9 @@
 package practice_fw.examples.component.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,7 +15,8 @@ public class HelloWorldController {
     super();
   }
 
-  @RequestMapping(value = "/hello_world.do", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+  // @RequestMapping(value = "/hello_world.do", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+  @GetMapping("/hello_world.do")
 
   @ResponseBody
   public String handleRequest() {
